ee4bc9269a806d08ae7332bd1af27b58c0d0ccbd
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-01-17 13:23:13 -0500

    Time: 1 ms (57.69%), Space: 20.5 MB (8.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,48 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        A=[]
-        for i, num in enumerate(nums):
-
-            A.append([num,i])
-    
-        A.sort()
-        i,j= 0, len(nums)-1
-
-        while(i<j):
-            current= A[i][0]+A[j][0]
-            if(current==target):
-                return[min(A[i][1],A[j][1]),max (A[i][1],A[j][1])]
-
-            elif current<target:
-                i+=1
-            else:
-                j-=1
+        indices={}
+        for i,n in enumerate (nums):
+            indices[n]=i
+        for i,n in enumerate (nums):
+            diff=target-nums[i]
+            if diff in indices and indices[diff]!=i:
+                return[i,indices[diff]] 
         return []
-            
 
-
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict1={}
-        for i, num in enumerate(nums):
-            dict1[num]=i
-        for i, num in enumerate(nums):
-            diff=target-num
-            if diff in dict1 and dict1[diff]!=i:
-                return [i, dict1[diff]]
-                
 
         
-    
-
-
-
-
-
-         
-
-
-    
-
-      
