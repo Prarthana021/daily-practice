88865af4d18865de1311046d753bd26dbcd7f2d5
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-01-18 21:38:12 -0500

    Time: 9 ms (89%), Space: 22.2 MB (32.68%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,6 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res=defaultdict(list)
+        #list is passed as a function 
+        #defaultdict will call list() whenever a key is missing
+        #{aet: [eat, tea], ant:[tan,ant]}
         for s in strs:
             sortedS= ''.join(sorted(s))
             res[sortedS].append(s)
