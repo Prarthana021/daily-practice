11f99f47275d10d9a4616ab37f1a2adbaa643ab0
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-01-18 21:37:07 -0500

    Time: 11 ms (83.23%), Space: 22.1 MB (35.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,14 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mydict={}
-        for str in strs:
-            #Since lists are unhashable (mutable), you can't use them as dictionary keys, which will cause an error. The fix:
-#Convert the sorted list back to a string using ''.join()
-            sorted_word = ''.join(sorted(str))
-
-            if sorted_word in mydict:
-                mydict[sorted_word].append(str)
-            else:
-                mydict[sorted_word]=[str]
-
-        return list(mydict.values())
+        res=defaultdict(list)
+        for s in strs:
+            sortedS= ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
