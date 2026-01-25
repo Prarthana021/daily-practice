6b3a4cae6e23c2dca454bf83a6cf267ab0dcf282
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-01-25 12:06:39 -0500

    Time: 222 ms (9.76%), Space: 20.8 MB (16.9%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,17 +1,21 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles)
-        res = r
+       l=1
+       r=max(piles)
+       res=r #because r cant exceed the max value in pile; kok cant eat more than max in the pile at once
+       while l<=r:
+        m=(l+r)//2
+        time = 0 
+        for i in piles:
+            time+=math.ceil(float(i) / m)
+        if time <= h:
+            res=min(res,m)
+            r=m-1
+        else:
+            l=m+1
+       return res
 
-        while l <= r:
-            k = (l + r) // 2
 
-            totalTime = 0
-            for p in piles:
-                totalTime += math.ceil(float(p) / k)
-            if totalTime <= h:
-                res = k
-                r = k - 1
-            else:
-                l = k + 1
-        return res
+
+        
+        
