691cd849bf4f982740850cb72f276aed05defc08
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-01-30 23:23:01 -0500

    Time: 11 ms (83.57%), Space: 22.1 MB (46.34%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,5 +1,19 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        mymap={}
+        for s in strs:
+            sortedS=''.join(sorted(s))
+            if sortedS in mymap:
+                mymap[sortedS].append(s)
+            else:
+                mymap[sortedS]=[s]
+        return list(mymap.values())
+
+
+
+
+
         res=defaultdict(list)
         #list is passed as a function 
         #defaultdict will call list() whenever a key is missing
