5e865b98ad2491d545148a1d28eb7b31ac328a1c
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-01-31 16:38:33 -0500

    Time: 7 ms (82.42%), Space: 19.7 MB (46.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+  
         l=0
         r=len(s)-1
         while l<r:
@@ -7,10 +8,9 @@ class Solution:
                 l+=1
             while l<r and not s[r].isalnum():
                 r-=1
-            if s[l].lower() != s[r].lower():
+            if s[l].lower()!=s[r].lower():
                 return False
             l=l+1
             r=r-1
         return True
 
-        
