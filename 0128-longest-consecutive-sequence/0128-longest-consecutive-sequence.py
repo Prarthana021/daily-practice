653fea1ba8636b132a60b74d46e87b558d4ad7f7
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-01-31 16:23:29 -0500

    Time: 36 ms (96.74%), Space: 36.9 MB (18.2%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,15 +1,16 @@
 #find if a number is start of a sequence , if yes find if num+1 exists in the set until it doesnt, in the meantime update the length counter accordingly; we will find until num+length not num+1 since length updates and again we need to check for next in the sequence 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-    
         numset=set(nums)
         longest=0
+
         for num in numset:
             if num-1 not in numset:
                 counter=1
                 while num+counter in numset:
-                    counter +=1
+                    counter+=1
                 longest=max(longest,counter)
         return longest
+
 
          
