0af38f80e86e39bccdabb1eb9f9c8f87ffe55d8f
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-02-02 16:01:06 -0500

    Time: 59 ms (73.53%), Space: 29.6 MB (32.1%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -6,7 +6,7 @@ class Solution:
         res=0
         while l<r:
             area= min(height[l],height[r])*(r-l) 
-            #main logic vanekai left and right ko minimum height chai line ho
+            #main logic vanekai left and right ko minimum height chai line ho while finding area
             res=max(res,area)
             if height[l]>=height[r]:
                 r-=1
