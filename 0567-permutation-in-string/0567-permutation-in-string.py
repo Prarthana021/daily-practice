6958b6c7e54bf643d6f65dd0fe9f18c59110dc16
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-02-08 10:36:56 -0500

    Time: 11 ms (96.84%), Space: 19.3 MB (50.46%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,29 +1,46 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        
         if len(s1)>len(s2):
             return False
-        
+
+
         s1arr=[0]*26
         s2arr=[0]*26
 
         for i in range(len(s1)):
-            indexs1=ord(s1[i])-ord('a')
-            indexs2=ord(s2[i])-ord('a')
+            indexs1= ord(s1[i])-ord('a')
+            indexs2= ord(s2[i])-ord('a')
 
             s1arr[indexs1]+=1
             s2arr[indexs2]+=1
-
+        
         for i in range(len(s2)-len(s1)):
-            if s1arr==s2arr:
+            if(s1arr==s2arr):
                 return True
-            
-            index_right = ord(s2[i+len(s1)])-ord('a')
-            index_left= ord(s2[i])-ord('a')
 
-            s2arr[index_right]+=1
-            s2arr[index_left]-=1
+            else:
+                indexr= ord(s2[i+len(s1)])-ord('a')
+                indexl=ord(s2[i])-ord('a')
 
+                s2arr[indexr]+=1
+                s2arr[indexl]-=1
+        
         return s2arr==s1arr
 
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
