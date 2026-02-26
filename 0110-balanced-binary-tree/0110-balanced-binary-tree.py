b3022457dab19b01f70a085006634578f8a963cd
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-02-26 09:44:13 -0500

    Time: 4 ms (30.54%), Space: 20.4 MB (85.32%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -8,10 +8,21 @@ class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def dfs(root):
             if not root:
-                return [True, 0]
+                return [True,0]
 
-            left, right = dfs(root.left), dfs(root.right)
-            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-            return [balanced, 1 + max(left[1], right[1])]
+            left=dfs(root.left)
+            right=dfs(root.right)
+
+            balanced= left[0] and right[0] and abs(left[1]-right[1]) <= 1
+            return[balanced, 1+max(left[1],right[1])]
 
         return dfs(root)[0]
+        # def dfs(root):
+        #     if not root: #node is root in function definition so same thing
+        #         return [True, 0]
+
+        #     left, right = dfs(root.left), dfs(root.right)
+        #     balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1 #whichever node you are at that node's right sub tree left sub tree and from that as root node check height difference
+        #     return [balanced, 1 + max(left[1], right[1])] #whichever node you are calculating by keeping it root that node's height value 
+
+        # return dfs(root)[0]
