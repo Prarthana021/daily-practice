c21cc5476cacc37dd72dc72f6d914b0626d33b6b
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-03-08 23:44:51 -0400

    Time: 84 ms (63.92%), Space: 20.8 MB (83.42%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,26 +1,36 @@
+#always try to run the task with the highest remaining frequency among the tasks that are currently allowed
+#1 increase time
+#2 run task from heap
+#3 add to queue
+#4 THEN check queue and move cooled tasks back to heap
+
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        count = Counter(tasks)
+        count=Counter(tasks)
+        maxheap= [-cnt for cnt in count.values()]
 
-        maxheap=[-cnt for cnt in count.values()]
         heapq.heapify(maxheap)
 
         time=0
-
         q=deque()
-        while maxheap or q:
-            time+=1
 
+        while maxheap or q:
+            time +=1
             if not maxheap:
-                time=q[0][1]    #first queue ko element ko time
+                time= q[0][1]
             else:
-                cnt=1+ heapq.heappop(maxheap)
-                if cnt: 
+                cnt= 1 + heapq.heappop(maxheap)
+
+                if cnt:
                     q.append([cnt,time+n])
             if q and q[0][1]==time:
                 heapq.heappush(maxheap,q.popleft()[0])
-        return time 
+
+        return time
+
+                
 
 
 
+        
         
