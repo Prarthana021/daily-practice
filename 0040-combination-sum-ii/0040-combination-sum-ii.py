007c6108f615a1a39290efbe079b845f7e9b7148
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-03-17 09:46:32 -0400

    Time: 37 ms (8.43%), Space: 19.5 MB (66.82%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -16,7 +16,8 @@ class Solution:
             cur.pop()
             while( i+1 < len(candidates) and candidates[i] == candidates[i + 1] ):
                 i +=1
-            dfs(i+1,cur, total) #skipping so no adding
+            dfs(i+1,cur, total) # The second call just passes the ball to the next candidate. THAT candidate will add to total if it gets taken inside its own call.
+
         
         dfs(0,[],0)
         return res
