6db154aa17b066230a3e9e65328747d0dd2fa716
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-03-22 16:13:42 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-  
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        ROWS, COLS = len(grid), len(grid[0])
-        islands = 0
 
-        def bfs(r, c):
-            q = deque()
-            grid[r][c] = "0"
-            q.append((r, c))
+        def dfs(grid,i,j):
+            if i < 0 or i >= len(grid):            # row is out of bounds (above or below grid)
+                return
+            if j < 0 or j >= len(grid[0]):      # col is out of bounds (left or right of grid)
+                return
+            if grid[i][j] == '0':                  # this cell is water OR already visited
+                return    
+            
+            grid[i][j]=0
 
-            while q:
-                row, col = q.popleft()
-                for dr, dc in directions:
-                    nr, nc = dr + row, dc + col
-                    if (nr < 0 or nc < 0 or nr >= ROWS or
-                        nc >= COLS or grid[nr][nc] == "0"
-                    ):
-                        continue
-                    q.append((nr, nc))
-                    grid[nr][nc] = "0"
+            dfs(grid,i-1,j)
+            dfs(grid,i+1,j)
+            dfs(grid, i,j-1)
+            dfs(grid, i,j+1)   
 
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == "1":
-                    bfs(r, c)
-                    islands += 1
+        count=0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]=='1':
+                    count+=1
+                    dfs(grid,i,j)
+                    
+                        
 
-        return islands
+        return count 
+        
