ca25538268563b6ca2ab6b01acf65756c80221a0
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-03-22 16:14:01 -0400

    Time: 238 ms (74.94%), Space: 21.7 MB (64.41%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -9,7 +9,7 @@ class Solution:
             if grid[i][j] == '0':                  # this cell is water OR already visited
                 return    
             
-            grid[i][j]=0
+            grid[i][j]='0'
 
             dfs(grid,i-1,j)
             dfs(grid,i+1,j)
