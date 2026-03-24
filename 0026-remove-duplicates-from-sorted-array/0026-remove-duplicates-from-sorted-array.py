0fb3c8f4d64c8073f06f99a2af69a941216ac799
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-03-24 13:33:52 -0400

    Time: 2 ms (49.41%), Space: 20.5 MB (90.86%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,7 +4,7 @@ class Solution:
         j=1
         while j < len(nums):
             if(nums[j]!=nums[i]):
-                i+=1.  #next place for unique element
+                i+=1 #next place for unique element
                 nums[i]=nums[j] #move unique element j is pointing to i 
                 
             j+=1
