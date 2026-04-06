645e6b9212409a56fee16ab626ea7b8c443a124d
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-04-06 11:27:32 -0400

    Time: 55 ms (5.02%), Space: 30 MB (94.36%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,16 +1,15 @@
 class Solution:
-    def containsDuplicate(self, nums:List[int])->bool:
-        myset=set()
-        for i in range(len(nums)):
-            if nums[i] in myset:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
                 return True
-            myset.add(nums[i])
         return False
-            
-#Or even compare the length of array and length of set by converting it to set
-#len(set(nums))==len(nums)
 
 
 
 
-            
+
+        
+
+        
