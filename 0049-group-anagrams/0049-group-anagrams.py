0ad063ed9058e8e2cdad9eb125a537704fbc3ade
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-04-22 11:57:35 -0400

    Time: 11 ms (84.6%), Space: 21.7 MB (97.9%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
+        res=[]
         seen={}
-        lst=[]
         for str in strs:
             new_str= "".join(sorted(str))
-            if new_str in seen:
-                seen[new_str].append(str)
-            else:
+            if new_str not in seen:
+                
                 seen[new_str]=[str]
-        return list(seen.values())
+            else:
+                seen[new_str].append(str)
+        
+        for item  in seen.values():
+            res.append(item)
+        return res
+        
+      
