aa689e727f13c864c5a80fcb1e54934d6f15d107
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-04-22 09:50:57 -0400

    Time: 0 ms (100%), Space: 13 MB (84.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,19 +1,20 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        hasmap={}
+        for i in range(len(nums)):
+            complement=target-nums[i]
+            if complement not in hasmap:
+                hasmap[nums[i]]=i
+            else:
+                return [hasmap.get(complement),i]
+
+
+     
+        
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        seen={}
-        for i in range(len(nums)):
-            
-            if (target-nums[i] not in seen):
-                seen[nums[i]]=i
-            else:
-                return[seen.get(target-nums[i]),i]
-        return[0,0]
-        
-        
-            
-        
+    
+                                                        
