5efd820e505220a242a6f16bd8edef346699bd0f
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-05-02 21:25:21 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -10,10 +10,10 @@ class Solution:
         for right in range(len(s)):
             mapS[s[right]]=mapS.get(s[right],0)+1
 
-            while self.contains(mapT,mapS):
-                if res=="" or right-left+1 < len(res):
-                    res=s[left:right+1]
-                mapS[s[left]]-=1
+            while self.contains(mapT,mapS): #for valid window
+                if res=="" or right-left+1 < len(res): #check and record minimum valid
+                    res=s[left:right+1]. #if min valid is available 
+                mapS[s[left]]-=1 # even if not if t in s then we shrink window and check again 
                 left+=1
         return res
 
@@ -23,49 +23,7 @@ class Solution:
             if mapT.get(ch,0)>mapS.get(ch,0):
                 return False
         return True
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #     mapT={}
-    #     mapS={}
-
-    #     left=0
-    #     res=""
-    #     for ch in t:
-    #         mapT[ch] = mapT.get(ch, 0) + 1
-
-    #     for right in range(len(s)):
-    #         mapS[s[right]]=mapS.get(s[right],0)+1
-    #         while self.contains(mapS,mapT):
-    #             mapS[s[left]]-=1
-    #             left+=1
-    #             if res=="" or right-left+1 < len(res):
-    #                 res=s[left:right+1]
-              
-    #     return res
     
-    # def contains(self, mapS, mapT):
-    #     for ch in mapT:
-    #         if mapT.get(ch,0)>mapS.get(ch,0):
-    #             return False
-    #     return True
-
-                
-
-        
- 
-
             
             
 
