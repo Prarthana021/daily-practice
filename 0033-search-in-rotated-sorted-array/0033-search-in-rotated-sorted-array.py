8b99eed37f44160dfe60f9cd2f558ffd84308a40
Prarthana021 <83604765+Prarthana021@users.noreply.github.com>
2026-05-11 12:51:16 -0400

    Time: 0 ms (100%), Space: 12.6 MB (45.13%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,27 +1,23 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        l,r=0,len(nums)-1
-        while l<=r:
-            mid = (l+r)//2
+class Solution(object):
+    def search(self, nums, target):
+        return self.modified_binary_search(nums, target, 0, len(nums) - 1)
 
-            #left sorted array -> what moves left to right
-            if target == nums[mid]:
-                return mid
-            if nums[l]<=nums[mid]:
-                if target>nums[mid] or target <nums[l]:
-                    l=mid+1
-                else:
-                    r=mid-1
-            #right sorted array -> what moves right to left 
+    def modified_binary_search(self, nums, target, left, right):
+        if left>right:
+            return -1
+        mid=(left+right) // 2
+        if nums[mid]==target:
+            return mid
+
+
+        if nums[mid] >= nums[left]:
+            if target >= nums[left] and target <= nums[mid]:
+                return self.modified_binary_search(nums, target, left, mid - 1)
             else:
-                if target < nums[mid] or target > nums[r]:
-                    r=mid-1
-                else:
-                    l= mid+1
-        return -1
-
-
-
-        
-
-        
+                return self.modified_binary_search(nums,target,mid+1,right)
+        else:
+            if target >= nums[mid] and target <= nums[right]:
+                return self.modified_binary_search(nums, target,mid+1,right)
+            else:
+                return self.modified_binary_search(nums, target,left,mid-1)
+            
